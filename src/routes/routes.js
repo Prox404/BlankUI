19b5dbe31df56eb1253a35a6e9358ebe9cb2d81b
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-01-30 00:31:51 +0700

    base form

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,11 +6,14 @@ import config from '~/configs';
 // Pages
 import Home from '~/pages/Home';
 import Cards from '~/pages/Cards';
+import Form from '~/pages/Form';
 
 // Public routes
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.card, component: Cards },
+    { path: config.routes.form, component: Form },
+
     // { path: config.routes.about, component: About, layout: HeaderOnly },
 ];
 
