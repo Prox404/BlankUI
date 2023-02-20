8f2a61c13072d52d8b113a02595c10311b226d85
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-02-20 21:39:04 +0700

    =))

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -5,6 +5,7 @@ function TextArea({
     Success = '',
     Info = '', 
     ...props }) {
+        // hom hay om nen nhat code
     return (<>
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
         <textarea rows="4" className="block p-2.5 w-full outline-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder ? placeholder : ' '}></textarea>
@@ -17,6 +18,7 @@ function TextArea({
         {
             Info && <p class="mt-2 text-sm text-blue-500 dark:text-blue-400">{Info}</p>
         }
+        
     </>);
 }
 
