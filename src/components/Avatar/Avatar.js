510ed4ab1a04cdc0f6f769501497e76e50f5435e
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-04-02 00:35:47 +0700

    update bordered avartar

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,10 +4,11 @@ function Avatar({
     size= 5,
     className,
     rounded='full',
+    bordered = false,
     ...props
 }) {
     return ( <>
-        <img src={src} alt={alt} className={`w-${size} h-${size}  ${rounded === 'full' ? 'rounded-full' : 'rounded-md'} ${className}`} {...props} />
+        <img src={src} alt={alt} className={`w-${size} h-${size}  ${rounded === 'full' ? 'rounded-full' : 'rounded-md'} ${className} ${bordered ? 'p-1 ring-2 ring-gray-300 dark:ring-gray-500' : ''}`} {...props} />
     </> );
 }
 
